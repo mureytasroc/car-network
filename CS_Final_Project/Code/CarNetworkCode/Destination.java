bd63ea17700fcce35a3306c1e4e543a447845076
sadigulcelik <sadi.gulcelik19@trinityschoolnyc.org>
2018-06-10 21:59:52 -0400

    !@#$

--- CS_Final_Project/Code/CarNetworkCode/Destination.java
+++ CS_Final_Project/Code/CarNetworkCode/Destination.java
@@ -5,12 +5,20 @@ public class Destination{
     Destination(Path p, double d){
         myPath=p;
         double distance=d;
-        spot=new Location((Point)myPath.getStart().getX+distance*myPath.getSlope())
+        double slope=myPath.getSlope();
+        double xinc=distance/Math.pow((1+slope*slope),0.5);
+        spot=new Location(p.getGrid(),((Point)myPath.getStart()).getX()+xinc,((Point)myPath.getStart()).getY()+xinc*slope);
+            
+            
         
     }
     Destination(Location l){
         myPath=l.snapToPath();
         spot=l;
+        double slope=myPath.getSlope();
+        double distance=Math.pow(Math.pow(((Point)myPath.getStart()).getX()-((Point)spot).getX(),2)
+                                 +Math.pow(((Point)myPath.getStart()).getY()-((Point)spot).getY(),2),0.5);
+        
         
     }
 }
